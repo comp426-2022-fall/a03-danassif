674fef9d19427c4e6726f82c067f8baf517919cb
David Nassif <danassif@live.unc.edu>
2022-11-21 17:23:01 -0500

    args and output calls in cli

--- bin/cli.js
+++ bin/cli.js
@@ -3,3 +3,9 @@ import minimist from "minimist";
 import { roll } from "/lib/roll.js"
 
 const args = minimist(process.argv.slice(2));
+
+let sides = args.sides;
+let dice = args.dice;
+let rolls = args.rolls;
+
+let output = roll(sides, dice, rolls);
